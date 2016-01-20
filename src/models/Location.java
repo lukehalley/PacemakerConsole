e866f71cc08437fb0661bd5038c03d5b45afb6dd
Luke Halley <luke123halley@gmail.com>
2016-01-20 19:19:34 +1000

    Add coordinate validation logic

--- src/models/Location.java
+++ src/models/Location.java
@@ -25,6 +25,7 @@ public class Location {
 		return toStringHelper(this).addValue(id).addValue(latitude).addValue(longitude).toString();
 	}
 
+// Latitude range: -90 to 90, Longitude range: -180 to 180
 	@Override
 	public int hashCode() {
 		return Objects.hashCode(this.id, this.latitude, this.longitude);
