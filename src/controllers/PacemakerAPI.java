e2250211c3960f3886b1728175a0b69c684bc669
Luke Halley <luke123halley@gmail.com>
2016-03-08 22:53:03 +1000

    Add TODO for API error handling

--- src/controllers/PacemakerAPI.java
+++ src/controllers/PacemakerAPI.java
@@ -39,6 +39,7 @@ public class PacemakerAPI {
 		User user = new User(firstName, lastName, email, password);
 		userIndex.put(user.id, user);
 		emailIndex.put(email, user);
+// TODO: Improve error handling for API endpoints
 		return user;
 	}
 
