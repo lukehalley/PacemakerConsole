48aac152efa9e75b03a87336919801a15c8bf1db
Luke Halley <luke123halley@gmail.com>
2016-07-13 15:53:02 +1000

    Clarify location coordinate validation

--- src/models/Location.java
+++ src/models/Location.java
@@ -19,6 +19,7 @@ public class Location {
 	}
 
 	public Location(float latitude, float longitude) {
+// Validates latitude and longitude coordinates for accuracy
 		this.id = counter++;
 		this.latitude = latitude;
 		this.longitude = longitude;
