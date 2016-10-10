cfe7aed0df24cd4638c2decdadcb34f7ebaf635b
Luke Halley <luke123halley@gmail.com>
2016-10-10 14:15:01 +0100

    Commented Out Fixtures

--- test/models/Fixtures.java
+++ test/models/Fixtures.java
@@ -1,29 +1,29 @@
-package models;
-
-public class Fixtures
-{
-  public static User[] users =
-  {
-    new User ("marge", "simpson", "marge@simpson.com",  "secret"),
-    new User ("lisa",  "simpson", "lisa@simpson.com",   "secret"),
-    new User ("bart",  "simpson", "bart@simpson.com",   "secret"),
-    new User ("maggie","simpson", "maggie@simpson.com", "secret")
-  };
-
-  public static Activity[] activities =
-  {
-    new Activity ("walk",  "fridge", 0.001),
-    new Activity ("walk",  "bar",    1.0),
-    new Activity ("run",   "work",   2.2),
-    new Activity ("walk",  "shop",   2.5),
-    new Activity ("cycle", "school", 4.5)
-  };
-
-  public static Location[]locations =
-  {
-    new Location(23.3f, 33.3f),
-    new Location(34.4f, 45.2f),  
-    new Location(25.3f, 34.3f),
-    new Location(44.4f, 23.3f)       
-  };
-}
+//package models;
+//
+//public class Fixtures
+//{
+//  public static User[] users =
+//  {
+//    new User ("marge", "simpson", "marge@simpson.com",  "secret"),
+//    new User ("lisa",  "simpson", "lisa@simpson.com",   "secret"),
+//    new User ("bart",  "simpson", "bart@simpson.com",   "secret"),
+//    new User ("maggie","simpson", "maggie@simpson.com", "secret")
+//  };
+//
+//  public static Activity[] activities =
+//  {
+//    new Activity ("walk",  "fridge", 0.001),
+//    new Activity ("walk",  "bar",    1.0),
+//    new Activity ("run",   "work",   2.2),
+//    new Activity ("walk",  "shop",   2.5),
+//    new Activity ("cycle", "school", 4.5)
+//  };
+//
+//  public static Location[]locations =
+//  {
+//    new Location(23.3f, 33.3f),
+//    new Location(34.4f, 45.2f),  
+//    new Location(25.3f, 34.3f),
+//    new Location(44.4f, 23.3f)       
+//  };
+//}
