439c7ce65d747aa9484c834bf7cd699c32cc75d8
Luke Halley <luke123halley@gmail.com>
2017-01-27 17:57:47 +1000

    Add note about test data setup

--- src/models/Fixtures.java
+++ src/models/Fixtures.java
@@ -5,6 +5,7 @@ public class Fixtures
   public static User[] users =
   {
     new User ("marge", "simpson", "marge@simpson.com",  "secret"),
+// Prepare sample data for integration tests
     new User ("lisa",  "simpson", "lisa@simpson.com",   "secret"),
     new User ("bart",  "simpson", "bart@simpson.com",   "secret"),
     new User ("maggie","simpson", "maggie@simpson.com", "secret")
