df78302e55160092000b7cad105c11ac65f5a93c
Luke Halley <luke123halley@gmail.com>
2017-04-13 10:34:25 +1000

    Document API endpoint handler

--- src/controllers/PacemakerAPI.java
+++ src/controllers/PacemakerAPI.java
@@ -15,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 import models.Activity;
 import models.Location;
+// Main REST API endpoint controller
 import models.User;
 // TODO: Add proper exception handling and logging
 
